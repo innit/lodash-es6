20cce26c663f2fccacbcaa55e9b0d0482a84e0fc
John-David Dalton <john.david.dalton@gmail.com>
2014-02-21 22:43:10 -0800

    Fix typo in `template` docs and tweak `zip` docs.

--- underscore/arrays/zip.js
+++ underscore/arrays/zip.js
@@ -19,7 +19,7 @@ import pluck from '../collections/pluck';
  * @memberOf _
  * @alias unzip
  * @category Arrays
- * @param {...Array} [array] Arrays to process.
+ * @param {...Array} [array] The arrays to process.
  * @returns {Array} Returns a new array of grouped elements.
  * @example
  *
